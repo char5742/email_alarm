c88facfdc2dc8157054c82d411c511db4600ca6c
char5742 <fujino@stellarcode.jp>
2024-01-20 00:20:54 +0900

    feat: add signin button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:email_alarm/providers/auth_service_provider.dart';
 import 'package:email_alarm/providers/config_provider.dart';
 import 'package:email_alarm/providers/email_alarm_service_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations([
     // Fixed to landscape
     DeviceOrientation.portraitUp,
--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,6 +1,7 @@
 import 'package:email_alarm/providers/auth_service_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SigninPage extends HookConsumerWidget {
@@ -11,14 +12,25 @@ class SigninPage extends HookConsumerWidget {
     useEffect(
       () {
         () async {
-          while (true) {
-            await ref.read(authServiceProvider).signIn();
-          }
+          await ref.read(authServiceProvider).signIn();
         }();
         return null;
       },
       const [],
     );
-    return const Scaffold();
+    final isLight = Theme.of(context).brightness == Brightness.light;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SignInButton(
+              isLight ? Buttons.Google : Buttons.GoogleDark,
+              onPressed: ref.read(authServiceProvider).signIn,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
