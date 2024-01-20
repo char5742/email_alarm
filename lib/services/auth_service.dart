ab5b095a4fc95bd1bbae65794ee96111cc52c18e
char5742 <fujino@stellarcode.jp>
2024-01-20 18:30:19 +0900

    fix: Enable forceCodeForRefreshToken for persistent refresh token authentication

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,6 +4,7 @@ import 'package:googleapis/gmail/v1.dart';
 class AuthService {
   AuthService();
   final googleSignIn = GoogleSignIn(
+    forceCodeForRefreshToken: true,
     scopes: [
       GmailApi.gmailReadonlyScope,
     ],
